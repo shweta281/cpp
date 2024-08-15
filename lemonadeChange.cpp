7492a8082512cb2fb4b548a767785b41786de149
shweta <132832323+shweta281@users.noreply.github.com>
2024-08-15 16:06:51 +0530

    Create lemonadeChange.cpp

---
+++ lemonadeChange.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0, ten=0;
+        for (int i=0; i<bills.size(); i++){
+            if(bills[i]==5) five++;
+            else if(bills[i]==10){
+                if(five==0) return false;
+                ten++;
+                five--;
+            }
+            else{
+                if(ten==0){
+                    if(five<3){
+                        return false;
+                    }
+                    five-=3;
+                }
+                else{
+                    if(five==0) return false;
+                    five--;
+                    ten--;
+                }
+            }
+        }
+        return true;
+    }
+};
